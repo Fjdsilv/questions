d6f33f80da147f50c7ac6110beeb07052d31c123
felipe silva <fjdsilv.dev@protonmail.com>
2024-05-21 19:00:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import data from "./data";
+import Questions from "./components/Questions";
+
+const App = () => {
+  const [questions, setQuestions] = useState(data);
+  const [activeID, setActiveID] = useState(null)
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeID ? null : id;
+    setActiveID(newActiveId)
+  }
+
+  return (
+    <main>
+      <Questions 
+        questions={questions}
+        activeID={activeID}
+        toggleQuestion={toggleQuestion} 
+      />
+    </main>
+  )
+}
+export default App
---
+++ src/components/Questions.jsx
@@ -0,0 +1,20 @@
+import SingleQuestion from "./SingleQuestion"
+
+const Questions = ({ questions, activeID, toggleQuestion }) => {
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+      {
+        questions.map((question) => {
+          return <SingleQuestion 
+                    key={question.id} 
+                    {...question}
+                    activeID={activeID}
+                    toggleQuestion={toggleQuestion}
+                 />
+        })
+      }
+    </section>
+  )
+}
+export default Questions
